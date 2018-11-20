614f63e98ba5334e35010e4932c87d2e54d9eac2
Jonas Gehrlein <jonas.gehrlein@iop.unibe.ch>
2018-11-20 11:07:23 +0100

    added toLine class

---
+++ Serien_P1/Serie_6/AddressFile.java
@@ -0,0 +1,22 @@
+public class AddressFile{
+    String filename;
+    String comma_string;
+
+
+
+     public AddressFile(String filename){
+        this.filename = filename;
+    }
+
+    private String toLine(Address addr){
+         // Gets all the information of an Address Object and puts them together as string with comma seperated
+        String comma_string = String.valueOf(addr.getId()) + "," + addr.getName() + "," + addr.getStreet() + "," + String.valueOf(addr.getZipCode()) + "," + addr.getCity();
+
+        return comma_string;
+    }
+
+    private parseLine(String line){
+
+    }
+
+}
---
+++ Serien_P1/Serie_6/mytest.java
@@ -0,0 +1,10 @@
+public class mytest{
+
+    public static void main(String[] args){
+        Address addr = new Address( 1, "Max Muster", "Neubrückstr. 10", 3012, "Bern" );
+
+
+
+
+    }
+}
