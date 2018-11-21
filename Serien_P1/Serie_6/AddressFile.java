06d7a6b8e673790137d605852e6f8d52bcdf2023
TheSaltyJ <Testing1>
2018-11-21 13:06:08 +0100

    made parseLine more efficient

--- Serien_P1/Serie_6/AddressFile.java
+++ Serien_P1/Serie_6/AddressFile.java
@@ -1,3 +1,9 @@
+/* ************************************************************************* *\
+*                Programmierung 1 HS 2018 - Serie 6-1                         *
+*		Jonas Gehrlein (15-127-541) & Jan Dietrich (10-100-436)	              *
+\* ************************************************************************* */
+
+
 import java.util.*; // Imports Scanner + ArrayList
 import java.io.*; // For FileWriter and IOException and File
 
@@ -22,20 +28,13 @@ public class AddressFile{
     /*Diese Methode liest einen String ein, löscht alle Leerzeichen mit trim() und geht dann mit next durch den String und zieht mit dem delimiter "," alles raus. Daraus wird dann ein
      * Address Object gebaut. */
     private Address parseLine(String line){
-
         Scanner s = new Scanner(line).useDelimiter(",");
-        String id_s = s.next();
-        id_s = id_s.trim();
-        int id = Integer.parseInt(id_s);
-        String name = s.next();
-        name = name.trim();
-        String street = s.next();
-        street = street.trim();
-        String zip_s = s.next();
-        zip_s = zip_s.trim();
-        int zip = Integer.parseInt(zip_s);
-        String city = s.next();
-        city = city.trim();
+
+        int id = Integer.parseInt((s.next().trim()));
+        String name = s.next().trim();
+        String street = s.next().trim();
+        int zip = Integer.parseInt((s.next().trim()));
+        String city = s.next().trim();
 
         Address parsed_address = new Address( id, name, street, zip, city );
         return parsed_address;
